10f04e933727c2ef66e6f0a0fa03a21dd649b12a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-14 00:27:07 -0400

    add the police thread

--- src/example2/Main.java
+++ src/example2/Main.java
@@ -69,4 +69,20 @@ public class Main {
             }
         }
     }
+
+    private static class PoliceThread extends Thread{
+        @Override
+        public void run(){
+            for (int i = 10; i > 0; i--){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+
+                }
+                System.out.println(i);
+            }
+            System.out.println("Game over for you hackers  ");
+            System.exit(0);
+        }
+    }
 }
