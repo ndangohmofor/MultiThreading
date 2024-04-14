e2873b84833516524a0b23c6151de7e0bcff1979
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-14 00:47:55 -0400

    run the program to pit the hackers against the police

--- src/example2/Main.java
+++ src/example2/Main.java
@@ -1,9 +1,23 @@
 package example2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static final int MAX_PASSWORD = 9999;
     public static void main(String[] args) {
+        Random random = new Random();
+        Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
 
+        List<Thread> threads = new ArrayList<>();
+        threads.add(new AscendingHackerThread(vault));
+        threads.add(new DescendingHackerThread(vault));
+        threads.add(new PoliceThread());
+
+        for (Thread thread: threads){
+            thread.start();
+        }
     }
 
     private static class Vault{
