b917d67471a07e046f322cc56420a2fbf522a807
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-14 10:09:24 -0400

    running a thread that catches Interrupted Exception

---
+++ src/threadTermination/Main.java
@@ -0,0 +1,23 @@
+package threadTermination;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread
+                .start();
+        thread
+                .interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+        @Override
+        public void run(){
+            //do things
+            try {
+                Thread.sleep(50000);
+            } catch (InterruptedException e) {
+                System.out.println("Existing blocking thread");
+            }
+        }
+    }
+}
