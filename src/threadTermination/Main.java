7fac904fef1ae48d24ccc507c5424df1b5a5da33
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-14 10:18:19 -0400

    add class that performs a long computation

--- src/threadTermination/Main.java
+++ src/threadTermination/Main.java
@@ -1,5 +1,7 @@
 package threadTermination;
 
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
         Thread thread = new Thread(new BlockingTask());
@@ -11,13 +13,37 @@ public class Main {
 
     private static class BlockingTask implements Runnable {
         @Override
-        public void run(){
+        public void run() {
             //do things
             try {
                 Thread.sleep(50000);
             } catch (InterruptedException e) {
                 System.out.println("Existing blocking thread");
             }
+        }
+    }
+
+    public static class LongComputation implements Runnable {
+
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputation(BigInteger base, BigInteger power){
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+            return result;
         }
     }
 }
