47f4957d380b8f5f6a84a9b37a3189a1d4c8c6a5
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-14 22:24:28 -0400

    testing the implementation

--- src/calculatePower/Main.java
+++ src/calculatePower/Main.java
@@ -1,2 +1,11 @@
-package calculatePower;public class Main {
+package calculatePower;
+
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        ComplexCalculation cal = new ComplexCalculation();
+        BigInteger result = cal.calculateResult(BigInteger.valueOf(100), BigInteger.valueOf(50), BigInteger.valueOf(500), BigInteger.valueOf(100));
+        System.out.println(result);
+    }
 }
