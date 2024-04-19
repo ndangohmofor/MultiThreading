2e3648cba5b3f13e69e8a6ad795078fb893ff021
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-19 07:04:53 -0400

    create http executor and pass into the http server

--- src/throughput/ThroughputHttpServer.java
+++ src/throughput/ThroughputHttpServer.java
@@ -6,9 +6,12 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ThroughputHttpServer {
     private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+    private static final int NUMBER_OF_THREADS = 1;
 
     public static void main(String[] args) throws IOException {
         String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
@@ -18,5 +21,8 @@ public class ThroughputHttpServer {
     public static void startServer(String text) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/search", new WordCountHandler(text));
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        server.setExecutor(executor);
+        server.start();
     }
 }
