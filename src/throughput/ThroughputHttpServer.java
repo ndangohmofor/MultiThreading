75d5f074d46d953ceca5043232ee3715c8616345
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-19 07:02:49 -0400

    create the http server

--- src/throughput/ThroughputHttpServer.java
+++ src/throughput/ThroughputHttpServer.java
@@ -1,6 +1,9 @@
 package throughput;
 
+import com.sun.net.httpserver.HttpServer;
+
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -10,5 +13,10 @@ public class ThroughputHttpServer {
     public static void main(String[] args) throws IOException {
         String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
         startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/search", new WordCountHandler(text));
     }
 }
