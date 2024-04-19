e322a6ed8394a4289ff656ce85630b58cfb278dd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-19 06:57:19 -0400

    create class to read the input file

---
+++ src/throughput/ThroughputHttpServer.java
@@ -0,0 +1,13 @@
+package throughput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ThroughputHttpServer {
+    private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+
+    public static void main(String[] args) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+    }
+}
