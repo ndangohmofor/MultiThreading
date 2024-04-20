b79b9d67b380db34543133f8409a2d55f22000a7
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-20 09:19:24 -0400

    in main method use the incrementing and decrementing thread counter classes to increment and decrement the inventory by equal amounts

--- src/thread/race/condition/Main.java
+++ src/thread/race/condition/Main.java
@@ -1,8 +1,18 @@
 package thread.race.condition;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
 
+        incrementingThread.start();
+        incrementingThread.join();
+
+        decrementingThread.start();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
     }
 
     public static class DecrementingThread extends Thread {
