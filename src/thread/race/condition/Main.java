9faa96ab0798daec5386f3e7401036576e52c700
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-20 09:16:49 -0400

    create decrementingthread which uses inventory counter to decrease inventory by 10000

--- src/thread/race/condition/Main.java
+++ src/thread/race/condition/Main.java
@@ -5,6 +5,21 @@ public class Main {
 
     }
 
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
     public static class IncrementingThread extends Thread {
         private InventoryCounter inventoryCounter;
 
