d8459f19d25b15514b54cddc4d0cb4dce71c7a04
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-21 03:10:36 -0400

    my minmax class for tracking the min and max values from a list

--- src/thread/race/condition/atomic/minmax/Main.java
+++ src/thread/race/condition/atomic/minmax/Main.java
@@ -1,2 +1,28 @@
-package thread.race.condition.atomic.minmax;public class Main {
+package thread.race.condition.atomic.minmax;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public class MinMaxMetrics {
+        private List<Long> sample;
+
+        public MinMaxMetrics(List<Long> sample) {
+            this.sample = sample;
+            Long.M
+        }
+
+        public void addSample(long newSample) {
+            this.sample.add(newSample);
+        }
+
+        public long getMin() {
+            return this.sample.stream().min(Comparator.naturalOrder()).get();
+        }
+
+        public long getMax() {
+            return this.sample.stream().max(Comparator.naturalOrder()).get();
+        }
+    }
 }
