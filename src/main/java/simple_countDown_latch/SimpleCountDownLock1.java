ae957456fdcfcf878a0fb6508b1c7baac43da084
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-28 23:03:01 -0400

    countdown latch using lock\'s condition variables

---
+++ src/main/java/simple_countDown_latch/SimpleCountDownLock1.java
@@ -0,0 +1,42 @@
+package simple_countDown_latch;
+
+public class SimpleCountDownLock1 {
+
+    private int count;
+
+    public SimpleCountDownLock1(int count) {
+        this.count = count;
+        if (this.count < 0) {
+            throw new IllegalArgumentException("Count cannot be negative");
+        }
+    }
+
+    /**
+     * Causes the current thread to wait until the latch has counted down to zero.
+     * If the current count is already zero then this method returns immediately.
+     */
+    public void await() throws InterruptedException {
+        /**
+         * Fill in your code
+         */
+    }
+
+    /**
+     * Decrements the count of the latch, releasing all waiting threads when the count reaches zero.
+     * If the current count already equals zero then nothing happens.
+     */
+    public void countDown() {
+        /**
+         * Fill in your code
+         */
+    }
+
+    /**
+     * Returns the current count.
+     */
+    public int getCount() {
+        /**
+         * Fill in your code
+         */
+    }
+}
