af6066e3030070a4dfdbe1cf197fec3dbc11bf3f
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-29 22:24:45 -0400

    create decrementing thread which decrements the counter

--- src/main/java/thread/race/condition/atomic/integer/Main.java
+++ src/main/java/thread/race/condition/atomic/integer/Main.java
@@ -2,6 +2,21 @@ package thread.race.condition.atomic.integer;
 
 public class Main {
 
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
     public static class InventoryCounter {
         private int items = 0;
 
