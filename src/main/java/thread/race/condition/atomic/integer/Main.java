24e72b2dd852d71d7773f28da389e1fa5981f678
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-29 22:26:48 -0400

    implement the incrementing thread class

--- src/main/java/thread/race/condition/atomic/integer/Main.java
+++ src/main/java/thread/race/condition/atomic/integer/Main.java
@@ -17,6 +17,21 @@ public class Main {
         }
     }
 
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
     public static class InventoryCounter {
         private int items = 0;
 
