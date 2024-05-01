af30b72927d8fee0e90d1d12877c21fc5b890851
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-30 20:05:26 -0400

    implement standard stack with push and pop methods

--- src/main/java/thread/atomic/reference/Main.java
+++ src/main/java/thread/atomic/reference/Main.java
@@ -1,2 +1,43 @@
-package thread.atomic.reference;public class Main {
+package thread.atomic.reference;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static class StandardStack<T> {
+        private StackNode<T> head;
+        private int counter = 0;
+
+        public synchronized void push(T value) {
+            StackNode<T> newHead = new StackNode<>(value);
+            newHead.next = head;
+            head = newHead;
+            counter++;
+        }
+
+        public synchronized T pop() {
+            if (head == null) {
+                counter++;
+                return null;
+            }
+            T value = head.value;
+            head = head.next;
+            counter++;
+            return value;
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+    }
+
+    private static class StackNode<T> {
+        public T value;
+        public StackNode<T> next;
+
+        public StackNode(T value) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
