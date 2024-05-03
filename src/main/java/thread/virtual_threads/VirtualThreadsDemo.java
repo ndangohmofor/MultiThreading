c40880642b91957f03b4d7a624ea604044e29c05
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-02 22:20:07 -0400

    create a pool of virtual threads to run on the platform threads simultaneously

--- src/main/java/thread/virtual_threads/VirtualThreadsDemo.java
+++ src/main/java/thread/virtual_threads/VirtualThreadsDemo.java
@@ -1,10 +1,27 @@
 package thread.virtual_threads;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VirtualThreadsDemo {
+    private static final int NUMBER_OF_VIRTUAL_THREADS = 2;
+
     public static void main(String[] args) throws InterruptedException {
         Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
-        Thread platformThread = Thread.ofPlatform().unstarted(runnable);
-        platformThread.start();
-        platformThread.join();
+
+        List<Thread> virtualThreads = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_VIRTUAL_THREADS; i++) {
+            Thread thread = Thread.ofVirtual().unstarted(runnable);
+            virtualThreads.add(thread);
+        }
+
+        for (Thread thread : virtualThreads) {
+            thread.start();
+        }
+
+        for (Thread thread : virtualThreads) {
+            thread.join();
+        }
     }
 }
