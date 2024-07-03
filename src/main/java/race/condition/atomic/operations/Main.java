406eb78845291c6410469cb8454808f0c7038c58
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-07-02 22:55:44 -0400

    add business logic that will add samples to the metrics object

--- src/main/java/race/condition/atomic/operations/Main.java
+++ src/main/java/race/condition/atomic/operations/Main.java
@@ -1,6 +1,30 @@
 package race.condition.atomic.operations;
 
+import java.util.Random;
+
 public class Main {
+
+    public static class BusinessLogic extends Thread {
+        private Metrics metrics;
+        private Random random = new Random();
+        public BusinessLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            long start = System.currentTimeMillis();
+
+            try {
+                Thread.sleep(random.nextInt(10));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            long end = System.currentTimeMillis();
+            metrics.addSample(end - start);
+        }
+    }
     public static class Metrics {
         private long count = 0;
         private volatile double average = 0.0;
