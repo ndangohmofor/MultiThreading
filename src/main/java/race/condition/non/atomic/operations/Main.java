030e8894573fdb7ccd0d0433e0d3e4c80f53f634
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-07-02 23:35:24 -0400

    add shared class to check for Data race condition

--- src/main/java/race/condition/non/atomic/operations/Main.java
+++ src/main/java/race/condition/non/atomic/operations/Main.java
@@ -1,2 +1,20 @@
-package race.condition.non.atomic.operations;public class Main {
+package race.condition.non.atomic.operations;
+
+public class Main {
+
+    public static class SharedClass {
+        private int x = 0;
+        private int y = 0;
+
+        public void increment(){
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace(){
+            if (y > x){
+                System.out.println("y > x -> Data race detected");
+            }
+        }
+    }
 }
