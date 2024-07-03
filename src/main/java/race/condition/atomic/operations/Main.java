d19c711959783d57f0e7010b5e91c45aa68b5f07
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-07-02 22:56:52 -0400

    add while true loop to run the business logic continuously

--- src/main/java/race/condition/atomic/operations/Main.java
+++ src/main/java/race/condition/atomic/operations/Main.java
@@ -7,24 +7,28 @@ public class Main {
     public static class BusinessLogic extends Thread {
         private Metrics metrics;
         private Random random = new Random();
+
         public BusinessLogic(Metrics metrics) {
             this.metrics = metrics;
         }
 
         @Override
         public void run() {
-            long start = System.currentTimeMillis();
+            while (true) {
+                long start = System.currentTimeMillis();
 
-            try {
-                Thread.sleep(random.nextInt(10));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                try {
+                    Thread.sleep(random.nextInt(10));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                long end = System.currentTimeMillis();
+                metrics.addSample(end - start);
             }
-
-            long end = System.currentTimeMillis();
-            metrics.addSample(end - start);
         }
     }
+
     public static class Metrics {
         private long count = 0;
         private volatile double average = 0.0;
